8580ad3018413d58104bc72ddc109ec9c512b04d
Ole Hansen <ole@jlab.org>
2023-08-13 14:54:33 -0400

    Add CMake scripts, LinkDef, .gitignore

---
+++ src/NPSlib_LinkDef.h
@@ -0,0 +1,14 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class THcNPSAnalyzer+;
+#pragma link C++ class THcNPSApparatus+;
+#pragma link C++ class THcNPSArray+;
+#pragma link C++ class THcNPSCalorimeter+;
+#pragma link C++ class THcNPSShowerHit+;
+#pragma link C++ class Decoder::VTPModule+;
+
+#endif
