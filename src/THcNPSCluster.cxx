72674af6ceb0ee0990f41850d1b3eaa60176e637
sanghwapark <sanghwapark@gmail.com>
2023-09-11 10:05:55 -0400

    THcNPSCluster: Add cluster time (Mark Mathison)

--- src/THcNPSCluster.cxx
+++ src/THcNPSCluster.cxx
@@ -3,24 +3,65 @@
 
 //______________________________________________________
 THcNPSCluster::THcNPSCluster() :
-  fE(0)
+  fE(0),
+  fT(0),
+  fHasVertex(false),
 {
   // Constructor
+
+  // Initialize momentum and vertex vectors
+  fPvect.SetXYZ(0., 0., 0.);
+  fVertex.SetXYZ(0., 0., 0.);
+  fCenterLab.SetXYZ(0., 0., 0.,);
 }
 
 //______________________________________________________
-THcNPSCluster::THcNPSCluster(Double_t x, Double_t y, Double_t z, Double_t e)
+THcNPSCluster::THcNPSCluster(Double_t x, Double_t y, Double_t z, Double_t e, Double_t t)
 {  
   fCenter.SetXYZ(x, y, z);
   fE = e;
+  fT = t;
+
+  fPvect.SetXYZ(0., 0., 0.);
+  fVertex.SetXYZ(0., 0., 0.);
+  fCenterLab.SetXYZ(0., 0., 0.,);
+  fHasVertex = false;
 }
 
 //______________________________________________________
 void THcNPSCluster::Clear( Option_t* )
-
 {
   fCenter.SetXYZ( kBig, kBig, kBig);
+  fPvect.SetXYZ( kBig, kBig, kBig);
+  fVertex.SetXYZ( kBig, kBig, kBig);
+  fCenterLab.SetXYZ( kBig, kBig, kBig);
+  fHasVertex = false;
   fE = 0;
+  fT = 0;
+}
+
+//______________________________________________________
+virtual void THcNPSCluster::RotateToLab(Double_t angle, TVector3& vertex, TVector3& pvect)
+{
+  // Set vertex vector
+  fVertex = vertex;
+  fHasVertex = true;
+
+  // Rotate along y-axis, correct for vertex
+  Double_t x_lab = fCenter.X()*cos(angle)  + fCenter.Z()*sin(angle) - vertex.X();
+  Double_t y_lab = fCenter.Y() - vertex.Y();
+  Double_t z_lab = -fCenter.X()*sin(angle) + fCenter.Z()*cos(angle) - vertx.Z();
+
+  TVector3 rvect(x_lab, y_lab, z_lab);
+  Double_t th = rvect.Theta();
+  Double_t ph = rvect.Phi();
+  
+  fCenterLab = rvect;
+  fPvect.SetXYZ(fE * cos(th) * sin(ph),
+	    fE * sin(th) * sin(ph),
+	    fE * cos(ph));
+
+  pvect = fPvect;
 }
 
 //______________________________________________________
--- src/THcNPSCluster.h
+++ src/THcNPSCluster.h
@@ -8,23 +8,44 @@
 //////////////////////////////////////
 
 #include "THaCluster.h"
+#include "TVector3.h"
 
 class THcNPSCluster : public THaCluster {
  public:
   THcNPSCluster();
-  THcNPSCluster(Double_t x, Double_t y, Double_t z, Double_t e);
+  THcNPSCluster(Double_t x, Double_t y, Double_t z, Double_t e, Double_t t);
   virtual ~THcNPSCluster() = default;
 
   virtual void Clear( Option_t* opt="" );
   virtual void Print( Option_t* opt="" ) const;
 
-  Double_t E() const { return fE; }
-  void     SetEnergy(Double_t e) { fE = e; }
+  // Coordinate transformation from detector plane to lab
+  virtual void RotateToLab(Double_t angle, TVector3& vertex, TVector3& pvect);
+
+  // Getter/Setter functions
+  bool       HasVertex()          const { return fHasVertex; }
+  Double_t   E()                  const { return fE; }
+  void       GetTime()                  { return fT; }
+  TVector3&  GetPvect()                 { return fPvect; } 
+  TVector3&  GetVertex()                { return fVertex; }
+  void       GetTheta()                 { return fPvect.Theta(); } // in lab frame
+  void       GetPhi()                   { return fPvect.Phi(); }   // in lab frame
+
+  void       SetEnergy(Double_t energy) { fE = energy; }
+  void       SetTime(Double_t time)     { fT = time; }
+  void       SetVertex(const TVector3& vertex) { fVertex = vertex; fHasVertex = true; }
+  void       SetVertex(Double_t vx, Double_t vy, Double_t vz) { fVertex.SetXYZ(vx, vy, vz); fHasVertex = true; }
+  void       SetPvect(const TVector3& pvect) { fPvect = pvect; }
 
  protected:
 
-  Double_t fE; // Cluster energy deposit
-  
+  Double_t fE;          // Cluster energy deposit
+  Double_t fT;          // Cluster time
+  bool     fHasVertex;   
+  TVector3 fVertex;     // vertex information from other spectrometer
+  TVector3 fPvect;      // momentum vector
+  TVector3 fCenterLab;  
+
   ClassDef(THcNPSCluster,0)
 
 };
